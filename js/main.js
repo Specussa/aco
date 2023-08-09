8b15e1ecdd232eb788b6c23a9b221ea4c0ff0599
Spec Ussa <serega.ussa@gmail.com>
2023-08-09 16:39:59 +0300

    fix point

--- js/main.js
+++ js/main.js
@@ -127,24 +127,24 @@ const mapalink = document.querySelectorAll('.map__list .map__link');
 
 function yamoscow() {
   if (idyamoscow.classList.contains("active")) {
-    myMap.setCenter([55.253215,37.622504],6);
+    myMap.setCenter([55.753215,37.622504],9);
     mapalink.forEach(n => n.classList.remove('active'));
     delmappoint();
   } else {
     mapalink.forEach(n => n.classList.remove('active'));
-    myMap.setCenter([55.88088924541,37.423524915344],12);
+    myMap.setCenter([55.715423,37.646013],17);
     idyamoscow.classList.add("active");
     document.querySelector('#mapmoscow').classList.add("map__active");
   };
 }
 function yamo() {
   if (idyamo.classList.contains("active")) {
-    myMap.setCenter([55.253215,37.622504],6);
+    myMap.setCenter([55.826867,37.120957],9);
     mapalink.forEach(n => n.classList.remove('active'));
     delmappoint();
   } else {
     mapalink.forEach(n => n.classList.remove('active'));
-    myMap.setCenter([54.647213,39.638701],12);
+    myMap.setCenter([55.826867,37.120957],17);
     idyamo.classList.add("active");
     document.querySelector('#mapmo').classList.add("map__active");
   };
@@ -167,8 +167,8 @@ if(maps) {
     myMap = document.getElementById("map");
     if (!myMap) return;
     myMap = new ymaps.Map(myMap, {
-      center: [55.253215, 37.622504],
-      zoom: 6, 
+      center: [55.753215, 37.622504],
+      zoom: 9, 
       controls: []
       },{
       zoomControlPosition: { right: 0, top: 0 },
@@ -200,20 +200,20 @@ if(maps) {
         "infoPoint": '<div id="mapmoscow" class="map__point{% if properties.active %} map__active{% endif %}">\
         <span class="map__icon"><svg width="21" height="26" viewBox="0 0 21 26" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.9289 13.429C11.6164 13.429 12.205 13.1842 12.6946 12.6946C13.1841 12.205 13.4289 11.6165 13.4289 10.929C13.4289 10.2415 13.1841 9.65291 12.6946 9.16333C12.205 8.67375 11.6164 8.42895 10.9289 8.42895C10.2414 8.42895 9.65289 8.67375 9.16331 9.16333C8.67373 9.65291 8.42893 10.2415 8.42893 10.929C8.42893 11.6165 8.67373 12.205 9.16331 12.6946C9.65289 13.1842 10.2414 13.429 10.9289 13.429ZM10.9289 22.6165C13.4706 20.2831 15.356 18.1633 16.5852 16.2571C17.8144 14.3508 18.4289 12.6581 18.4289 11.179C18.4289 8.90812 17.705 7.04875 16.2571 5.60083C14.8091 4.15291 13.0331 3.42895 10.9289 3.42895C8.82477 3.42895 7.04873 4.15291 5.60081 5.60083C4.15289 7.04875 3.42893 8.90812 3.42893 11.179C3.42893 12.6581 4.04352 14.3508 5.27268 16.2571C6.50185 18.1633 8.38727 20.2831 10.9289 22.6165ZM10.9289 25.929C7.57477 23.0748 5.06956 20.4237 3.41331 17.9758C1.75706 15.5279 0.928934 13.2623 0.928934 11.179C0.928934 8.05395 1.93414 5.56437 3.94456 3.71021C5.95498 1.85604 8.2831 0.928955 10.9289 0.928955C13.5748 0.928955 15.9029 1.85604 17.9133 3.71021C19.9237 5.56437 20.9289 8.05395 20.9289 11.179C20.9289 13.2623 20.1008 15.5279 18.4446 17.9758C16.7883 20.4237 14.2831 23.0748 10.9289 25.929Z" fill="currentColor"/></svg></span>\
           <div class="map__point_block">\
-            <div class="map__point_temp">ул. Бабакина, д. 5-А, оф. 707</div>\
+            <div class="map__point_temp">115114, г. Москва, Павелецкая наб, 2-с1, офис 102</div>\
           </div>\
         </div>',
-        "latitude": 55.88088924541,
-        "longitude": 37.423524915344,
+        "latitude": 55.715423,
+        "longitude": 37.646013,
         },{
         "infoPoint": '<div id="mapmo" class="map__point{% if properties.active %} map__active{% endif %}">\
         <span class="map__icon"><svg width="21" height="26" viewBox="0 0 21 26" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.9289 13.429C11.6164 13.429 12.205 13.1842 12.6946 12.6946C13.1841 12.205 13.4289 11.6165 13.4289 10.929C13.4289 10.2415 13.1841 9.65291 12.6946 9.16333C12.205 8.67375 11.6164 8.42895 10.9289 8.42895C10.2414 8.42895 9.65289 8.67375 9.16331 9.16333C8.67373 9.65291 8.42893 10.2415 8.42893 10.929C8.42893 11.6165 8.67373 12.205 9.16331 12.6946C9.65289 13.1842 10.2414 13.429 10.9289 13.429ZM10.9289 22.6165C13.4706 20.2831 15.356 18.1633 16.5852 16.2571C17.8144 14.3508 18.4289 12.6581 18.4289 11.179C18.4289 8.90812 17.705 7.04875 16.2571 5.60083C14.8091 4.15291 13.0331 3.42895 10.9289 3.42895C8.82477 3.42895 7.04873 4.15291 5.60081 5.60083C4.15289 7.04875 3.42893 8.90812 3.42893 11.179C3.42893 12.6581 4.04352 14.3508 5.27268 16.2571C6.50185 18.1633 8.38727 20.2831 10.9289 22.6165ZM10.9289 25.929C7.57477 23.0748 5.06956 20.4237 3.41331 17.9758C1.75706 15.5279 0.928934 13.2623 0.928934 11.179C0.928934 8.05395 1.93414 5.56437 3.94456 3.71021C5.95498 1.85604 8.2831 0.928955 10.9289 0.928955C13.5748 0.928955 15.9029 1.85604 17.9133 3.71021C19.9237 5.56437 20.9289 8.05395 20.9289 11.179C20.9289 13.2623 20.1008 15.5279 18.4446 17.9758C16.7883 20.4237 14.2831 23.0748 10.9289 25.929Z" fill="currentColor"/></svg></span>\
           <div class="map__point_block">\
-            <div class="map__point_temp">ул. Бабакина, д. 5-А, оф. 707</div>\
+            <div class="map__point_temp">деревня Лобаново, 256</div>\
           </div>\
         </div>',
-        "latitude": 54.647213,
-        "longitude": 39.638701,
+        "latitude": 55.826867,
+        "longitude": 37.120957,
         },
       ],
     };
@@ -255,15 +255,15 @@ if(maps) {
 
       var mapmoscow = document.getElementById('mapmoscow');
       if (mapmoscow.classList.contains("map__active")) {
-        myMap.setCenter([55.88088924541,37.423524915344],12);
+        myMap.setCenter([55.715423,37.646013],17);
         idyamoscow.classList.add("active");
       } else {
-        myMap.setCenter([55.253215,37.622504],6);
+        myMap.setCenter([55.753215,37.622504],9);
         mapalink.forEach(n => n.classList.remove('active'));
       };
       var mapmo = document.getElementById('mapmo');
       if (mapmo.classList.contains("map__active")) {
-        myMap.setCenter([54.647213,39.638701],12);
+        myMap.setCenter([55.826867,37.120957],17);
         idyamo.classList.add("active");
       } else {};
     });
